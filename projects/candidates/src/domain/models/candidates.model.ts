8542e3c8f44c4503a914cb2a51755f8681f4469d
Sebastian barrera <sebasbh2008@hotmail.com>
2025-04-23 19:18:34 -0500

    feat: :construction: adding structure

---
+++ projects/candidates/src/domain/models/candidates.model.ts
@@ -0,0 +1,7 @@
+export interface ICandidate {
+    id: string;
+    name: string;
+    email: string;
+    party: string;
+    votes: number;
+}
---
+++ projects/candidates/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of candidates
+ */
+
+export * from './lib/candidates.service';
+export * from './lib/candidates.component';
